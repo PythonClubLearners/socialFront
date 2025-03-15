81536958e83d10ff7dec4493067483ca01c4d088
AutoUser <autoUser@IKNWNTHEPASSWORD.pythonanywhere.com>
2025-03-15 11:57:37 +0300

    colors + header

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,34 @@ import { PostPage } from "./pages/PostPage"
 import { CreatePostPage } from "./pages/CreatePostPage"
 import { NotFound } from "./pages/NotFound"
 
+import './css/colors.css'
+import './css/fonts.css'
+import './css/App.css'
+
+import IconImage from './img/icon.svg'
+import { ContextHeaderButton } from "./components/ContextHeaderButton"
 
 export default function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path="/auth/login" element={<LoginPage/>}/>
-        <Route path="/users/:id" element={<ProfilePage/>}/>
-        <Route path="/users/:id/friends" element={<FriendsPage/>}/>
-        <Route path="/friend-requests" element={<FriendsRequestsPage/>}/>
-        <Route path="/posts/new" element={<CreatePostPage/>}/>
-        <Route path="/posts/:id" element={<PostPage/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </Router>
+      <div className={"App"}>
+        <header>
+          <img id="logo" src={IconImage}/>
+          <ContextHeaderButton/>
+        </header>
+        <Router>
+              <Routes>
+                <Route path="/" element={<MainPage/>}/>
+                <Route path="/auth/login" element={<LoginPage/>}/>
+                <Route path="/users/:id" element={<ProfilePage/>}/>
+                <Route path="/users/:id/friends" element={<FriendsPage/>}/>
+                <Route path="/friend-requests" element={<FriendsRequestsPage/>}/>
+                <Route path="/posts/new" element={<CreatePostPage/>}/>
+                <Route path="/posts/:id" element={<PostPage/>}/>
+                <Route path="*" element={<NotFound/>}/>
+              </Routes>
+            </Router>
+
+      </div>
   )
 }
---
+++ src/components/ContextHeaderButton.jsx
@@ -0,0 +1,3 @@
+export function ContextHeaderButton(){
+    return <button>ВОЙТИ</button>
+}
