438ae2e2a0e211a5299742d7a8a431fa58057775
AutoUser <autoUser@IKNWNTHEPASSWORD.pythonanywhere.com>
2025-03-20 18:00:26 +0300

    login styles

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,20 @@
 export function LoginPage() {
     return (
-        <div className="page login">
-            Login
+        <div className="page login vertical-list align-center">
+            <form className="vertical-list">
+                <h1>
+                    Готовы войти?
+                </h1>
+                <label htmlFor="login">
+                    Логин
+                </label>
+                <input id="login" type="text" placeholder="Логин"/>
+                <label htmlFor="password">
+                    Пароль
+                </label>
+                <input id="password" type="password" placeholder="Пароль"/>
+                <button type="submit" className={"primary"}>ВОЙТИ</button>
+            </form>
         </div>
     );
 }
