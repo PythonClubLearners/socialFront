5ec6ba9934ea451d95f8a968042059c63bc38daa
AutoUser <autoUser@IKNWNTHEPASSWORD.pythonanywhere.com>
2025-03-21 14:12:16 +0300

    prepare mock data

---
+++ src/api/mock_data.js
@@ -0,0 +1,103 @@
+export const mock_user_1 = {
+
+    id: 3,
+
+    username: "gooduser",
+
+    firstName: "Иван",
+
+    lastName: "Тестович",
+
+    description: "Это пример страницы пользователя",
+
+    avatar: null,
+
+}
+
+
+export const mock_user_2 = {
+
+    id: 4,
+
+    username: "frienduser",
+
+    firstName: "Иван",
+
+    lastName: "Дружелюбный",
+
+    description: "Это пример страницы пользователя",
+
+    avatar: null,
+
+}
+
+  
+  
+
+export const mock_all_users = [
+
+    mock_user_1, mock_user_2
+
+]
+
+  
+
+export const mock_post_1 = {
+
+    id: 1,
+
+    image: null,
+
+    title: "Пост первый",
+
+    description: "Что то тут написано",
+
+    author: 3,
+
+}
+
+  
+  
+
+export const mock_post_2 = {
+
+    id: 2,
+
+    image: "defaultImage",
+
+    title: "Пост второй",
+
+    description: "Что то тут написано",
+
+    author: 3,
+
+}
+
+  
+  
+
+export const mock_posts = [
+
+    mock_post_1,
+
+    mock_post_2
+
+]
+
+  
+
+export const mock_friends_invite = [
+
+    3,
+
+    4
+
+]
+
+  
+
+export const mock_friends = [
+
+    4
+
+]
