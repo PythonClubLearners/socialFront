fc82b4a1a2f5312fa36fc68c66b5816c01c53330
AutoUser <autoUser@IKNWNTHEPASSWORD.pythonanywhere.com>
2025-03-22 11:14:21 +0300

    friends request  page

--- src/components/UserCollection.jsx
+++ src/components/UserCollection.jsx
@@ -5,7 +5,13 @@ import { UserLink } from '../components/UserLink'
 export function UserCollection({acceptButton}){
     const users = mock_friends_invite
 
-    return users.map(
-        (user)=><UserLink acceptButton={acceptButton}/>
-    )
+    return (
+        <div className='user-list vertical-list xfill'>
+            {
+                users.map(
+                    (user)=><UserLink acceptButton={acceptButton}/>
+                )
+            }
+        </div>
+    );
 }
--- src/pages/FriendsRequestsPage.jsx
+++ src/pages/FriendsRequestsPage.jsx
@@ -1,7 +1,12 @@
+import { UserCollection } from "../components/UserCollection";
+
 export function FriendsRequestsPage() {
     return (
-        <div className="page friend-requests">
-            Friends request
-        </div>
+        <div className="page friends-requests vertical-list xfill align-center">
+        <h1>
+            Заявки в друзья
+        </h1>
+        <UserCollection acceptButton={true}/>
+    </div>
     );
 }
